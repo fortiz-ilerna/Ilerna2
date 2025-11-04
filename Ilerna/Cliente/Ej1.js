a2e96ed915e37c659e227cc42a0592b6389b6a6e
Francisco Ortiz Luna <fortiz@ilerna.com>
2025-11-04 08:53:49 +0100

    modifico el 1 y añado ejercicio 3

--- Ilerna/Cliente/Ej1.js
+++ Ilerna/Cliente/Ej1.js
@@ -8,10 +8,7 @@ let numero2 = 5;
 
 let suma = numero1 + numero2;
 let resta = numero1 - numero2;
-let multiplicacion = numero1 * numero2;
-let division = numero1 / numero2;
 
 console.log("Suma:", suma);
 console.log("Resta:", resta);
-console.log("Multiplicación:", multiplicacion);
-console.log("División:", division);
+
