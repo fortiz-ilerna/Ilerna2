ff2bd0210541519e60f8d0c51777df43b99422e2
Francisco Ortiz Luna <fortiz@ilerna.com>
2025-11-04 08:51:59 +0100

    termino ejercicios 1 y 2

---
+++ Ilerna/Cliente/Ej1.js
@@ -0,0 +1,17 @@
+/*Ejercicio 1: Operaciones Aritméticas Básicas
+Escribe un programa que declare dos variables numéricas, 
+les asigne valores y muestre por pantalla el resultado de sumarlas, 
+restarlas, multiplicarlas y dividirlas.*/
+
+let numero1 = 10;
+let numero2 = 5;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Suma:", suma);
+console.log("Resta:", resta);
+console.log("Multiplicación:", multiplicacion);
+console.log("División:", division);
---
+++ Ilerna/Cliente/Ej2.js
@@ -0,0 +1,17 @@
+// Ejercicio 2: Uso de Constantes
+/*Declara una constante llamada GRAVEDAD 
+con el valor de 9.8. Calcula y muestra la fuerza 
+de un objeto de masa m = 10 kg utilizando esta 
+constante (Fuerza = masa × gravedad).*/
+
+// Declarar la constante de gravedad
+const GRAVEDAD = 9.8; // m/s²
+
+// Declarar la masa del objeto
+let masa = 10; // kg
+
+// Calcular la fuerza
+let fuerza = masa * GRAVEDAD;
+
+// Mostrar el resultado por pantalla
+console.log("La fuerza del objeto es:", fuerza, "N"); // N = Newtons
